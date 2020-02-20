7dd824dbe7c3a9b7fc5a4d05ad2a0cba21cbc9bd
Sofialav <cemeteryvixen@gmail.com>
2020-02-20 14:31:22 +0100

    card logic fixed

--- src/store/cart/cartReducer.js
+++ src/store/cart/cartReducer.js
@@ -4,17 +4,23 @@ export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case "ADD_TO_CART": {
       if (state.length > 0) {
-        return state.map(obj => {
-          if (obj.id === action.payload.id) {
-            console.log("id equal", obj);
-            return { ...obj, qty: obj.qty + 1 };
-          } else {
-            return obj;
-          }
-        });
-      } else {
-        return [...state, action.payload];
+        // return state.map(obj => {
+        //   if (obj.id === action.payload.id) {
+        //     console.log("id equal", obj);
+        //     return { ...obj, qty: obj.qty + 1 };
+        //   } else {
+        //     return obj;
+        //   }
+        // });
+        const x = state.find(obj => obj.id === action.payload.id);
+        if (x) {
+          const y = state.filter(obj => obj !== x);
+          return [...y, { ...x, qty: x.qty + 1 }];
+        } else {
+          return [...state, action.payload];
+        }
       }
+      return [...state, action.payload];
     }
     default:
       return state;
